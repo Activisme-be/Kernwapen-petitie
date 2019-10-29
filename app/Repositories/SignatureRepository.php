e46a1078e4b031b5d92768ceb46b8ad20ad8a1b9
Tim Joosten <Tjoosten3@gmail.com>
2019-10-29 11:35:09 +0100

    Update

--- app/Repositories/SignatureRepository.php
+++ app/Repositories/SignatureRepository.php
@@ -22,6 +22,8 @@ class SignatureRepository
     /**
      * Method for storing an signature in the application.
      *
+     * @todo. Signatures on the petition should associated to the postal code that the quest has filled id
+     *
      * @param  array $data The data that comes off the signature form.
      * @return Signature
      */
